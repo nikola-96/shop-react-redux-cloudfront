e356c12ae717c288e157c76455d74078b8ab7e11
nikola-96 <nikolaviki@hotmail.com>
2023-02-27 16:20:47 +0100

    added right api values

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,9 +1,11 @@
+const { VITE_API_BASE_URL: API_BASE_URL } = import.meta.env;
+
 const API_PATHS = {
-  product: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  import: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  bff: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  cart: "https://.execute-api.eu-west-1.amazonaws.com/dev",
+  product: API_BASE_URL,
+  order: API_BASE_URL,
+  import: API_BASE_URL,
+  bff: API_BASE_URL,
+  cart: API_BASE_URL,
 };
 
 export default API_PATHS;
